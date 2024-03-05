a2cbe133dc738edc62a4f6fdd01011cb0805c039
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-05 21:39:00 +0900

    Chore: 불필요한 레이아웃 파일 삭제

--- src/app/about/layout.tsx
+++
@@ -1,7 +0,0 @@
-type AboutLayoutProps = {
-  children: React.ReactNode;
-};
-
-export default function AboutLayout({ children }: AboutLayoutProps) {
-  return <main className="mx-auto flex min-h-dvh bg-white">{children}</main>;
-}

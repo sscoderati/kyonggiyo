671634cdea3927817142c48180c79c048da469c8
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-05 21:35:56 +0900

    Feat: 홈에 맵, 마커, 마커 상세 모달 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import MarkerDialog from "@/components/Dialog/MarkerDialog";
+import KakaoMap from "@/components/Map/KakaoMap";
+import Markers from "@/components/Map/Markers";
+import NavBar from "@/components/NavBar";
+
 export default function Home() {
   return <main className="mx-auto flex min-h-dvh bg-white">Home</main>;
+  const [map, setMap] = useState(null);
+  const [selectedMarker, setSelectedMarker] = useState(null);
+  return (
+    <>
+      <NavBar />
+      <KakaoMap setMap={setMap} />
+      <Markers
+        map={map}
+        setSelected={setSelectedMarker}
+      />
+      <MarkerDialog
+        selected={selectedMarker}
+        setSelected={setSelectedMarker}
+      />
+    </>
+  );
 }

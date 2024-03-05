2b813ca91724fd6a659a9ecec43c5aa635e2382e
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-05 21:23:47 +0900

    Feat: UserStore 정의
    - zustand persist

---
+++ src/store/UserStore.ts
@@ -0,0 +1,26 @@
+import type { TokenResponse } from "@/types";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface UserStore {
+  accountId: number | null;
+  token: TokenResponse | null;
+  // eslint-disable-next-line no-unused-vars
+  setAccountId: (accountId: number) => void;
+  // eslint-disable-next-line no-unused-vars
+  setToken: (token: TokenResponse) => void;
+}
+
+export const useUserStore = create(
+  persist<UserStore>(
+    (set) => ({
+      accountId: null,
+      token: null,
+      setAccountId: (accountId: number) => set({ accountId }),
+      setToken: (token: TokenResponse) => set({ token }),
+    }),
+    {
+      name: "user-storage",
+    },
+  ),
+);

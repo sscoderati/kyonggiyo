641fbccbad79a3ebcbd220e0356a2ba691526a3d
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-05 22:28:09 +0900

    Fix: 불필요한 return문 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import Markers from "@/components/Map/Markers";
 import NavBar from "@/components/NavBar";
 
 export default function Home() {
-  return <main className="mx-auto flex min-h-dvh bg-white">Home</main>;
   const [map, setMap] = useState(null);
   const [selectedMarker, setSelectedMarker] = useState(null);
   return (

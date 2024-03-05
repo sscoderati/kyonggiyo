349aa8a08526e6b30b11ed41af756d77c5f9921f
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-05 21:32:15 +0900

    Chore: Nav바에 사이드바 추가 외 1건
    - 로고 옆 Label에 홈으로 이동하는 링크 연결

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+import SideBar from "@/components/SideBar/SideBar";
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 
@@ -7,14 +9,16 @@ export default function NavBar() {
     <div
       className={"flex h-12 w-full items-center justify-between bg-gray-100"}
     >
-      <div className={"ml-4 flex items-center"}>
+      <div className={"ml-4 flex w-full items-center"}>
         <Image
           src={"/assets/logo.svg"}
           alt={"logo"}
           width={32}
           height={32}
         />
-        <div className={"font-bold text-blue-500"}>경기요!</div>
+        <Link href={"/"}>
+          <div className={"font-bold text-blue-500"}>경기요!</div>
+        </Link>
       </div>
       <Button
         className={"mr-4"}
@@ -23,6 +27,7 @@ export default function NavBar() {
       >
         <Menu className={"h-4 w-4"} />
       </Button>
+      <SideBar />
     </div>
   );
 }

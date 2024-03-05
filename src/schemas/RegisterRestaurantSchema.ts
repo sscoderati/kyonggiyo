282554ae03abb9275364d3b266a268d54e4dda9d
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-05 21:56:41 +0900

    Chore: 맛집 등록 신청 폼 데이터 스키마 추가

---
+++ src/schemas/RegisterRestaurantSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const RegisterRestaurantSchema = z.object({
+  name: z.string({ required_error: "맛집 이름 입력은 필수입니다!" }).min(2, {
+    message: "맛집 이름은 최소 2글자 이상이어야 합니다.",
+  }),
+  category: z.string({ required_error: "맛집 카테고리 선택은 필수입니다!" }),
+  contact: z.string().optional(),
+  address: z.string({ required_error: "맛집 주소 입력은 필수입니다!" }),
+  lat: z.number(),
+  lng: z.number(),
+});
+
+export type RegisterRestaurantSchemaType = z.infer<
+  typeof RegisterRestaurantSchema
+>;

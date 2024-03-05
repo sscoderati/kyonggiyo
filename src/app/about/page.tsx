8a7aef0c66bf70739f5eb017ae108915e8ae531e
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-05 22:00:15 +0900

    Chore: 소개 페이지, 맛집 신청 현황 페이지 Route 추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,10 @@
+import NavBar from "@/components/NavBar";
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <NavBar />
+      <div>서비스 소개 페이지</div>
+    </>
+  );
 }
---
+++ src/app/restaurant/status/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import NavBar from "@/components/NavBar";
+
+export default function RestaurantDetailPage() {
+  return (
+    <>
+      <NavBar />
+      <div className={"h-dvh w-full p-4"}>
+        <h1 className={"text-xl font-bold"}>맛집 신청 현황</h1>
+      </div>{" "}
+    </>
+  );
+}

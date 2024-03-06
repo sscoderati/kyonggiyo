2b137d10f770c59912a297175a9eddb995249d25
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-06 17:15:27 +0900

    Chore: 토큰 가져오는 방식 수정

--- src/hooks/useUserAuthLevel.ts
+++ src/hooks/useUserAuthLevel.ts
@@ -1,7 +1,7 @@
 import { useUserStore } from "@/store/UserStore";
 
 const useUserAuthLevel = () => {
-  const { token } = useUserStore();
+  const token = useUserStore.getState().token;
 
   if (token) {
     const payload = JSON.parse(atob(token.accessToken.split(".")[1]));

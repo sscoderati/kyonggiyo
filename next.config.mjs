8c4088f85e0d80e9291f8db80f8ca22a4c5dc031
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-06 21:18:16 +0900

    Chore: s3 이미지 remotePattern 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'kyonggiyo-bucket.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+      }
+    ]
+  }
+};
 
 export default nextConfig;

eda1cf2eeffca475963d440bc951b9e22b10b523
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-06 21:21:08 +0900

    Chore: Navbar에 임시 토큰 재발행 버튼 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
+import getNewToken from "@/apis/getNewToken";
 import SideBar from "@/components/SideBar/SideBar";
 import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
+import { RotateCw } from "lucide-react";
+import { toast } from "sonner";
+import { useUserStore } from "@/store/UserStore";
 
 export default function NavBar() {
+  const { setToken } = useUserStore();
+  const handleRefreshToken = () => {
+    getNewToken().then((res) => {
+      if (res) {
+        setToken(res);
+        toast.success("토큰 재발행 성공!");
+      }
+    });
+  };
+
   return (
     <div
       className={"flex h-12 w-full items-center justify-between bg-gray-100"}
@@ -22,10 +35,11 @@ export default function NavBar() {
       </div>
       <Button
         className={"mr-4"}
-        variant={"outline"}
         size={"icon"}
+        variant={"outline"}
+        onClick={handleRefreshToken}
       >
-        <Menu className={"h-4 w-4"} />
+        <RotateCw className={"h-4 w-4"} />
       </Button>
       <SideBar />
     </div>

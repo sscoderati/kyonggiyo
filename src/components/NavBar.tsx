0de744f5a90ca9b2b8eed2568f0cf89e072cfec2
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-08 12:34:48 +0900

    Chore: 불필요한 토큰 재발행 버튼 제거

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
-import getNewToken from "@/apis/getNewToken";
 import SideBar from "@/components/SideBar/SideBar";
-import { Button } from "@/components/ui/button";
-import { RotateCw } from "lucide-react";
-import { toast } from "sonner";
-import { useUserStore } from "@/store/UserStore";
 
 export default function NavBar() {
-  const { setToken } = useUserStore();
-  const handleRefreshToken = () => {
-    getNewToken().then((res) => {
-      if (res) {
-        setToken(res);
-        toast.success("토큰 재발행 성공!");
-      }
-    });
-  };
-
   return (
     <div
       className={"flex h-12 w-full items-center justify-between bg-gray-100"}
@@ -33,14 +18,6 @@ export default function NavBar() {
           <div className={"font-bold text-blue-500"}>경기요!</div>
         </Link>
       </div>
-      <Button
-        className={"mr-4"}
-        size={"icon"}
-        variant={"outline"}
-        onClick={handleRefreshToken}
-      >
-        <RotateCw className={"h-4 w-4"} />
-      </Button>
       <SideBar />
     </div>
   );

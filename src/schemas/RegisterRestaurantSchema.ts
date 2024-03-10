8f4d5f7be0b18bc8f380a0856950be0807aa27ae
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-11 00:17:32 +0900

    Fix: 맛집 등록 폼, 리뷰 작성 폼 데이터 유효성 검증 로직 수정

--- src/schemas/RegisterRestaurantSchema.ts
+++ src/schemas/RegisterRestaurantSchema.ts
@@ -1,11 +1,15 @@
 import { z } from "zod";
 
 export const RegisterRestaurantSchema = z.object({
-  name: z.string({ required_error: "맛집 이름 입력은 필수입니다!" }),
+  name: z
+    .string({ required_error: "맛집 이름 입력은 필수입니다!" })
+    .min(1, { message: "맛집 이름 입력은 필수입니다!" }),
   category: z.string({ required_error: "맛집 카테고리 선택은 필수입니다!" }),
   contact: z.string().optional(),
   reason: z.string(),
-  address: z.string({ required_error: "맛집 주소 입력은 필수입니다!" }),
+  address: z
+    .string({ required_error: "맛집 주소 입력은 필수입니다!" })
+    .min(1, { message: "맛집 주소 입력은 필수입니다!" }),
   lat: z.number(),
   lng: z.number(),
 });
--- src/schemas/ReviewWriteForm.ts
+++ src/schemas/ReviewWriteForm.ts
@@ -1,8 +1,12 @@
 import { z } from "zod";
 
 export const ReviewWriteFormSchema = z.object({
-  rating: z.number({ required_error: "평점 입력은 필수입니다!" }),
-  content: z.string({ required_error: "리뷰 내용 입력은 필수입니다!" }),
+  rating: z
+    .number({ required_error: "평점 입력은 필수입니다!" })
+    .min(1, { message: "평점 입력은 필수입니다!" }),
+  content: z
+    .string({ required_error: "리뷰 내용 입력은 필수입니다!" })
+    .min(1, { message: "리뷰 내용 입력은 필수입니다!" }),
   images: z.array(z.any()).optional(),
 });
 

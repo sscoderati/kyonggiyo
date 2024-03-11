b7811126d0fb9db01d152576e3792cd27fccfda2
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-11 11:17:07 +0900

    Feat: 맛집 주소 검색 란에 지도 앱 열기 링크 추가
    - 카카오맵 추후 작업 예정

--- src/app/candidate/register/page.tsx
+++ src/app/candidate/register/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useForm } from "react-hook-form";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import postRestaurant from "@/apis/postRestaurant";
 import postRestaurantCandidate from "@/apis/postRestaurantCandidate";
@@ -18,12 +19,39 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { APPNAME, KGU_ENCODED_NAME, KGU_POS } from "@/constants";
+import { cn } from "@/lib/utils";
 import type { RegisterRestaurantSchemaType } from "@/schemas/RegisterRestaurantSchema";
 import { RegisterRestaurantSchema } from "@/schemas/RegisterRestaurantSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { toast } from "sonner";
 import checkUserAuthLevel from "@/utils/checkUserAuthLevel";
 import { RestaurantCategories } from "@/constants/selectorOptions";
+
+const MAP_LOGO_SIZE = 32;
+const INTENT_NAVER_MAP = `intent://place?lat=${KGU_POS.lat}&lng=${KGU_POS.lng}&name=${KGU_ENCODED_NAME}&appname=${APPNAME}#Intent;scheme=nmap;action=android.intent.action.VIEW;category=android.intent.category.BROWSABLE;package=com.nhn.android.nmap;end`;
+
+const handleOpenNaverMapApp = () => {
+  const userOS = window.navigator.userAgent.match(/Android/i)
+    ? "android"
+    : "ios";
+  if (userOS === "android") {
+    window.location.href = INTENT_NAVER_MAP;
+    return;
+  }
+
+  if (userOS === "ios") {
+    const clickedAt = +new Date();
+
+    window.location.href = `nmap://actionPath?parameter=value&place?lat=${KGU_POS.lat}&lng=${KGU_POS.lng}&name=${KGU_ENCODED_NAME}&appname=${APPNAME}`;
+
+    setTimeout(function () {
+      if (+new Date() - clickedAt < 2000) {
+        window.location.href = "http://itunes.apple.com/app/id311867728?mt=8";
+      }
+    }, 1500);
+  }
+};
 
 export default function CandidateRegisterPage() {
   const router = useRouter();
@@ -117,7 +145,25 @@ export default function CandidateRegisterPage() {
                 </FormMessage>
               </FormItem>
               <FormItem className={"w-full"}>
-                <FormLabel>맛집 주소</FormLabel>
+                <div className={cn("flex w-full justify-between")}>
+                  <FormLabel className={"my-auto"}>맛집 주소</FormLabel>
+                  <div className={"mr-2 flex gap-3"}>
+                    <Image
+                      src={"/images/naver_map.png"}
+                      alt={"네이버맵_로고"}
+                      width={MAP_LOGO_SIZE}
+                      height={MAP_LOGO_SIZE}
+                      className={"cursor-pointer"}
+                      onClick={handleOpenNaverMapApp}
+                    />
+                    <Image
+                      src={"/images/kakao_map.png"}
+                      alt={"카카오맵_로고"}
+                      width={MAP_LOGO_SIZE}
+                      height={MAP_LOGO_SIZE}
+                    />
+                  </div>
+                </div>
                 <FormControl>
                   <div className={"flex w-full gap-2"}>
                     <Input
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,3 +8,6 @@ export const SideBarRoutes = [
 export const PrivateRoutes = ["/candidate/register", "/candidate/status"];
 
 export const KGU_POS = { lat: 37.300548, lng: 127.03577 };
+export const KGU_ENCODED_NAME =
+  "%ea%b2%bd%ea%b8%b0%eb%8c%80%ed%95%99%ea%b5%90+%ec%88%98%ec%9b%90%ec%ba%a0%ed%8d%bc%ec%8a%a4";
+export const APPNAME = "com.sscoderati.kyonggiyo";

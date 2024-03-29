00a7457c6f87e242dc628de2006a78bc7fa426f0
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-30 02:48:48 +0900

    Chore: React Query Devtools 추가

--- src/lib/react-query/QueryClient.tsx
+++ src/lib/react-query/QueryClient.tsx
@@ -2,6 +2,7 @@
 
 import type { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,7 +15,10 @@ const queryClient = new QueryClient({
 
 const RQProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
   );
 };
 

99acdca25ebe2a4d5623ee85ddb73f343f4a6d8f
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-31 01:09:13 +0900

    Chore: KGU_POS에 서울캠 좌표 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,7 +6,10 @@ export const SideBarRoutes = [
 
 export const PrivateRoutes = ["/candidate/register", "/candidate/status"];
 
-export const KGU_POS = { lat: 37.300548, lng: 127.03577 };
+export const KGU_POS = {
+  suwon: { lat: 37.300548, lng: 127.03577 },
+  seoul: { lat: 37.5649767, lng: 126.9623605 },
+};
 export const KGU_ENCODED_NAME =
   "%ea%b2%bd%ea%b8%b0%eb%8c%80%ed%95%99%ea%b5%90+%ec%88%98%ec%9b%90%ec%ba%a0%ed%8d%bc%ec%8a%a4";
 export const APPNAME = "com.sscoderati.kyonggiyo";
